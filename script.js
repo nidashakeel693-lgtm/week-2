3ab030557c89522df7dbd3705e8d98e864116b64
Nida Shakeel <nidashakeel693@gmail.com>
2025-12-17 13:04:54 +0500

    added new code

--- script.js
+++ script.js
@@ -27,4 +27,21 @@ document.addEventListener('DOMContentLoaded', function () {
     form.reset(); // clear form after submit
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopBtn = document.getElementById("backToTop");
 
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  });
+
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
